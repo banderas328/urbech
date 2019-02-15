409b185e3b904b4985097b353addb03461aa4783
Anton Zhauryd <banderas328@yandex.ru>
2019-02-15 22:27:08 +0300

    some work

--- news/views.py
+++ news/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import News
+from django.core import serializers
+from django.shortcuts import render
+from django.template.response import TemplateResponse
 
-# Create your views here.
+
+def getmorenews(request):
+    news_id = request.POST.get('news_id')
+    news = News.objects.filter(id__gt=news_id)[:2]
+    response = TemplateResponse(request, 'partials/news.html', {'news': news})
+    return response
--- urbech/urls.py
+++ urbech/urls.py
@@ -3,8 +3,10 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 import pages.views
+import news.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', pages.views.home, name='home.html'),
+    path('', pages.views.home, name='home'),
+    path('getmorenews', news.views.getmorenews, name='getmorenews'),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
